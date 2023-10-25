2555b8b4f469bb0a94053054b694fbb9a3a81409
bazyli12 <bazylixyz@gmail.com>
2023-10-25 23:48:05 +0200

    Create dashboard shell

--- apps/frontend/src/app/(dashboard)/layout.tsx
+++ apps/frontend/src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import DashboardShell from "@/components/dashboard-shell";
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 import { Container } from "@/components/ui/container";
@@ -6,10 +7,12 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="flex h-screen flex-row bg-gray-50">
             <Sidebar />
-            <Container className="inline-flex flex-col gap-6 py-5">
+            <div className="w-full flex flex-col">
                 <Navbar />
-                {children}
-            </Container>
+                <DashboardShell>
+                    {children}
+                </DashboardShell>
+                </div>
         </div>
     );
 };
---
+++ apps/frontend/src/components/dashboard-shell.tsx
@@ -0,0 +1,5 @@
+const DashboardShell = ({ children }: {children: React.ReactNode}) => {
+    return <div className="pt-7 px-8">{children}</div>
+}
+
+export default DashboardShell;
